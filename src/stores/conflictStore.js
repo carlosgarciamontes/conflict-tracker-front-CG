43f52af9a0c5ab67c431b15d8915a06b2df51340
carlosgarciamontes <carlosgarciamontes@elpuig.xeill.net>
2026-04-29 12:22:21 +0200

    fix error

--- src/stores/conflictStore.js
+++ src/stores/conflictStore.js
@@ -31,7 +31,7 @@ export const useConflictStore = defineStore('conflict', {
       if(!confirm('¿Seguro que quieres borrar este conflicto?')) return;
 
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/conflicts/%{id}`, {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/conflicts/${id}`, {
           method: 'DELETE'
         });
         

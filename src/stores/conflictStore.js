83fda011423bfd6ef6943ee86fb13f08be9c8429
carlosgarciamontes <carlosgarciamontes@elpuig.xeill.net>
2026-04-29 12:23:39 +0200

    add conflict

--- src/stores/conflictStore.js
+++ src/stores/conflictStore.js
@@ -1,24 +1,26 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useConflictStore = defineStore('conflict', {
+export const useConflictStore = defineStore("conflict", {
   state: () => ({
     conflicts: [],
     isLoading: false,
-    error: null
+    error: null,
   }),
 
   getters: {
-    totalConflicts: (state) => state.conflicts.length
+    totalConflicts: (state) => state.conflicts.length,
   },
 
   actions: {
     async fetchConflicts() {
       this.isLoading = true;
       this.error = null;
-      
+
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/conflicts`);
-        if (!response.ok) throw new Error('Error al conectar con la API');
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/v1/conflicts`,
+        );
+        if (!response.ok) throw new Error("Error al conectar con la API");
         this.conflicts = await response.json();
       } catch (err) {
         this.error = "No se pudieron cargar los conflictos";
@@ -27,20 +29,49 @@ export const useConflictStore = defineStore('conflict', {
       }
     },
 
+    async addConflict(newConflict) {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/v1/conflicts`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newConflict),
+          },
+        );
+
+        if (response.ok) {
+          const savedConflict = await response.json();
+
+          this.conflicts.push(savedConflict);
+          return true;
+        }
+        throw new Error("Error al guardar en el servidor");
+      } catch (err) {
+        console.error("No se pudo añadir el conflicto:", err);
+        return false;
+      }
+    },
+
     async deleteConflict(id) {
-      if(!confirm('¿Seguro que quieres borrar este conflicto?')) return;
+      if (!confirm("¿Seguro que quieres borrar este conflicto?")) return;
 
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/conflicts/${id}`, {
-          method: 'DELETE'
-        });
-        
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/v1/conflicts/${id}`,
+          {
+            method: "DELETE",
+          },
+        );
+
         if (response.ok) {
-          this.conflicts = this.conflicts.filter(c => c.id !== id);
+          this.conflicts = this.conflicts.filter((c) => c.id !== id);
         }
       } catch (err) {
         console.error(err);
       }
-    }
-  }
-})
+    },
+  },
+});
